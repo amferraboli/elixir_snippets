058ab0a5ab0f7d9557f8358611a626888fba65f8
Breno Silva <breno.batista@wildlifestudios.com>
2021-10-08 00:21:50 -0300

    feature: add pattern match for division by 0

--- src/calculator.exs
+++ src/calculator.exs
@@ -1,48 +1,54 @@
 defmodule Calculator do
   alias :math, as: Math
 
-  def sum(x,y) do
+  def sum(x, y) do
     x + y
   end
 
-  def difference(x,y) do
-    x-y
+  def difference(x, y) do
+    x - y
   end
 
-  def product(x,y) do
-    x*y
+  def product(x, y) do
+    x * y
   end
 
-  def power(x,y) do
-    Math.pow(x,y)
+  def power(x, y) do
+    Math.pow(x, y)
   end
 
-  def quotient(x,y) do
-    #to do: div by zero guard
-    x/y
+  def quotient(x, y) do
+    # to do: div by zero guard
+    x / y
   end
 
   def calculate("Sum", x, y) do
-    sum(x,y)
+    sum(x, y)
   end
 
   def calculate("Sub", x, y) do
-    difference(x,y)
+    difference(x, y)
   end
 
   def calculate("Mul", x, y) do
-    product(x,y)
+    product(x, y)
   end
 
-  def calculate("Div", x, y) do
-    quotient(x,y)
+  def calculate("Div", x, y) when y > 0 or y < 0 do
+    quotient(x, y)
+  end
+
+  def calculate("Div", x, _) do
+    IO.puts("Sorry, can't divide by 0. Please, choose another second number.")
+    y = Operation.choose_number_2()
+    Calculator.calculate("Div", x, y)
   end
 
   def calculate("Pow", x, y) do
-    power(x,y)
+    power(x, y)
   end
 
-  def calculate(_, x, y) do
+  def calculate(_, _, _) do
     {:error, "Invalid operation"}
   end
 end
@@ -56,44 +62,44 @@ end
 defmodule Welcome do
   def initiate_calculator do
     IO.puts("Welcome to Elixir Calculator! It can help you with simple and complex calculations.")
-    IO.puts("Available functions: ")
+    IO.puts("Available functions:")
     Timer.sleep()
-    IO.puts("Sum returns the sum of two elements")
+    IO.puts("- Sum returns the sum of two elements.")
     Timer.sleep()
-    IO.puts("Subtraction returns the difference of two elements")
+    IO.puts("- Subtraction returns the difference of two elements.")
     Timer.sleep()
-    IO.puts("Multiplication returns the product of two elements")
+    IO.puts("- Multiplication returns the product of two elements.")
     Timer.sleep()
-    IO.puts("Division returns the quotient of two elements")
+    IO.puts("- Division returns the quotient of two elements.")
     Timer.sleep()
-    IO.puts("Power returns an element to the power of the other")
+    IO.puts("- Power returns an element to the power of the other.")
     Timer.sleep()
   end
 end
 
 defmodule Operation do
   def choose_number_1 do
-    "What is the first number of your operation?"
-    #to do: type check valid number
-    |>IO.gets()
-    |>String.trim()
-    |>Float.parse()
+    "What is the first number of your operation? "
+    # to do: type check valid number
+    |> IO.gets()
+    |> String.trim()
+    |> Float.parse()
     |> elem(0)
   end
 
   def choose_operation do
-    "What operation do you want to perform? Choose one of the following Sum, Sub, Mul, Div or Pow."
-    |>IO.gets()
-    |>String.trim()
-    |>String.capitalize()
+    # "What operation do you want to perform? Choose one of the following Sum, Sub, Mul, Div or Pow."
+    "What operation do you want to perform? [Sum, Sub, Mul, Div or Pow] "
+    |> IO.gets()
+    |> String.trim()
+    |> String.capitalize()
   end
 
   def choose_number_2 do
-    "What is the second number of your operation?"
-    #to do: type check valid number
-    |>IO.gets()
-    |>String.trim()
-    |>Float.parse()
+    "What is the second number of your operation? "
+    |> IO.gets()
+    |> String.trim()
+    |> Float.parse()
     |> elem(0)
   end
 
@@ -101,7 +107,6 @@ defmodule Operation do
     number_1 = choose_number_1()
     operation = choose_operation()
     number_2 = choose_number_2()
-    check_zero_div(operation, number_2)
     result = Calculator.calculate(operation, number_1, number_2)
     IO.puts("Your result is #{result}")
   end
@@ -109,17 +114,17 @@ end
 
 defmodule NextStep do
   def end_of_operation do
-    "Do you want to perform another operation? [Y / N]."
-    |>IO.gets()
-    |>String.trim()
-    |>String.capitalize()
+    "Do you want to perform another operation? [Y / N] "
+    |> IO.gets()
+    |> String.trim()
+    |> String.capitalize()
   end
 
-  def next_step_choice(another_operation) when another_operation == "Y" do
+  def next_step_choice("Y") do
     CompleteOperation.complete_operation()
   end
 
-  def next_step_choice(another_operation) when another_operation == "N" do
+  def next_step_choice("N") do
     IO.puts("Thanks for using the Elixir Calculator")
   end
 end
@@ -127,9 +132,10 @@ end
 defmodule CompleteOperation do
   def complete_operation do
     Operation.result()
+
     NextStep.end_of_operation()
-    |>NextStep.next_step_choice()
-    end
+    |> NextStep.next_step_choice()
+  end
 end
 
 Welcome.initiate_calculator()
