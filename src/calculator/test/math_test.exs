80c97c37a4f20bc75adc48e0fbe4a89430ce4f4a
Breno Silva <breno.batista@wildlifestudios.com>
2021-10-31 20:55:27 -0300

    fix: typo from test descriptions

--- src/calculator/test/math_test.exs
+++ src/calculator/test/math_test.exs
@@ -6,7 +6,7 @@ defmodule Calculator.MathTest do
     assert Calculator.Math.sum(2, 2) == {:ok, 4}
   end
 
-  test "should return 0 when subtract 2 by 2" do
+  test "should return 0 when subtract 2 from 2" do
     assert Calculator.Math.difference(2, 2) == {:ok, 0}
   end
 
@@ -14,7 +14,7 @@ defmodule Calculator.MathTest do
     assert Calculator.Math.product(2, 2) == {:ok, 4}
   end
 
-  test "should return 9 when power 3 by 2" do
+  test "should return 9 when power 3 to 2" do
     assert Calculator.Math.power(3, 2) == {:ok, 9}
   end
 
